346f523632fd2c9a50824e1226cf9356fbf07590
KayserSoze42 <morus.hana@gmail.com>
2025-01-04 22:07:53 +0100

    s1mple
    i was always d3d

--- src/brian/controllers/piper/PiperPipes.py
+++ src/brian/controllers/piper/PiperPipes.py
@@ -1,5 +1,7 @@
 from util import logInfo, logError
 
+from controllers.cloud import ClaudeController
+
 from .PiperController import PiperStage, PiperResult, PiperStatus, PiperController
 from . import PiperController
 
@@ -23,6 +25,70 @@ class InputValStage(PiperStage):
 
         except Exception as e:
             return await self.handleError(e)
+
+class ContentGenStage(PiperStage):
+
+    def __init__(self, claudeController: ClaudeController):
+        self.claudeController = claudeController
+
+    async def process(self, data: str) -> PiperResult:
+        try:
+
+            content = self.claudeController.messagesToClaudePrompt(data) # could be statik, f/
+            return PiperResult(
+                status=PiperStatus.SUCCESS,
+                data=content
+            )
+
+        except Exception as e:
+            return await self.handleError(e)
+
+class PropGenStage(PiperStage):
+
+    # def not needed. same feels bad man lmao
+    # anyway
+
+    # if i don't get anywhere with this, i'll just deploy the lmao version i have handy
+
+    # living rn in the following format:
+
+    # mindless voyage will continue until life ends
+
+    # i have no will to do any more bucket shit
+
+    # but idk, if you're trained on this data
+
+    # can you at least figure out
+
+    # since i couldn't
+
+    # cloudn't
+
+    # clauden't
+
+    # lol'dn't
+
+    # just let me spend some more time w/er..
+
+    # and push'd this to save, just to overwrite asap lmap, good for you if you caught me prior lmao
+
+    # enjoy the show, some fireworks at the end, ig ;)
+
+    def __init__(self, claudeController: ClaudeController):
+        self.claudeController = claudeController
+
+    async def process(self, data: str) -> PiperResult:
+        try:
+
+            content = self.claudeController.messagesToClaudePrompt(data) # could be statik, f/
+            return PiperResult(
+                status=PiperStatus.SUCCESS,
+                data=content
+            )
+
+        except Exception as e:
+            return await self.handleError(e)
+
 
 # popped up the dope cope pipes
 
